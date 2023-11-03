bd3dc0e36c3f1e703e0d618705de832e77bb270a
Emma Nasseri <enasseri02@gmail.com>
2023-11-02 22:48:13 -0500

    added images

--- src/app/cs/page.js
+++ src/app/cs/page.js
@@ -14,6 +14,25 @@ export default function CSPage() {
           imagePath="/images/spencerImage.png" // Path to your image
           alt="Description for Accessibility" // Alt text for the image
         />
+        <OutlineCard
+          title="Blockchain Attendance Tracker"
+          description="a student focused attendance tool"
+          imagePath="/images/spencerImage.png" // Path to your image
+          alt="Description for Accessibility" // Alt text for the image
+          link="https://attendthis.on.fleek.co/"
+        />
+        <OutlineCard
+          title="Philosophical Chatbot"
+          description="talk with philosophers"
+          imagePath="/images/spencerImage.png" // Path to your image
+          alt="Description for Accessibility" // Alt text for the image
+        />
+        <OutlineCard
+          title="Interdisciplinary collaborations"
+          description="a choose your own adventure game"
+          imagePath="/images/spencerImage.png" // Path to your image
+          alt="Description for Accessibility" // Alt text for the image
+        />
       </div>
     </>
   );
--- src/components/OutlineCard.js
+++ src/components/OutlineCard.js
@@ -1,17 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import OutlineButton from "./OutlineButton";
 
-const OutlineCard = ({ title, bodyText, imagePath }) => {
+const OutlineCard = ({ title, description, imagePath, link }) => {
   return (
-    <div style={styles.button}>
-      <Image
-        src={imagePath}
-        alt="Description for Accessibility"
-        width={500}
-        height={500}
-      />
+    <div style={styles.card}>
+      <div style={styles.imageContainer}>
+        <Image
+          src={imagePath}
+          alt="Description for Accessibility"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
       <span style={styles.title}>{title}</span>
-      <span style={styles.body}>{bodyText}</span>
+      <span style={styles.body}>{description}</span>
+      <OutlineButton title="view project" onClick={link} />
     </div>
   );
 };
@@ -19,15 +23,32 @@ const OutlineCard = ({ title, bodyText, imagePath }) => {
 export default OutlineCard;
 
 const styles = {
-  button: {
+  card: {
     border: "2px solid black",
     borderRadius: "8px",
     padding: "8px 16px",
     backgroundColor: "transparent",
     color: "black",
-    cursor: "pointer",
     outline: "none",
     transition: "0.3s",
+    width: "500px", // adjust as needed
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  imageContainer: {
+    width: "100%",
+    height: "250px", // adjust as needed
+    position: "relative",
+    marginBottom: "8px", // space between image and title
+  },
+  title: {
+    fontWeight: "bold",
+    marginBottom: "8px", // space between title and body
+  },
+  body: {
+    fontSize: "0.9em",
   },
   link: {
     textDecoration: "none",
